a604b687978b2a5a192f87a6f8a4f1b1c4dc311d
Samara Emilly <samara.emilly2013@gmail.com>
2023-05-11 15:46:41 -0300

    Project created

---
+++ src/test/java/com/devsuperior/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
