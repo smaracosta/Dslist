c0b9e0ec9737f75a84df89f0fe09b607e9e0dd06
Samara Emilly <samara.emilly2013@gmail.com>
2023-05-12 17:09:34 -0300

    GET name bu id

--- src/main/java/com/devsuperior/DSList/services/GameService.java
+++ src/main/java/com/devsuperior/DSList/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.DSList.dto.GameDTO;
 import com.devsuperior.DSList.dto.GameMinDTO;
 import com.devsuperior.DSList.entities.Game;
 import com.devsuperior.DSList.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)  // Essa annotation garante que o método garanta que a operação com o BD aconteça.
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
